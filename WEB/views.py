6a5bfd89826f347a229b3f9decb6c6eeace28182
Yusdivia Molina <yus.molina@gmail.com>
2021-04-19 22:48:36 -0500

    Update views.py

--- WEB/views.py
+++ WEB/views.py
@@ -170,7 +170,7 @@ def exitoPromedio(request):
 
 @csrf_exempt
 def scoresGlobal(request):
-    p =Day.objects.select_related('try_id')
+    p =Day.objects.select_related('try_id', 'session_id')
     print(p)
 
 
