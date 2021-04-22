7f5da01e96126d399f0ba1690cb5c4872dfcc695
Yusdivia Molina <yus.molina@gmail.com>
2021-04-22 12:57:04 -0500

    Update views.py

--- WEB/views.py
+++ WEB/views.py
@@ -124,7 +124,8 @@ def SendLoginData(request):
     jugador_pass = body['data_b']
     print(jugador_pass)
     jugador_o  = User.objects.filter(username=jugador_nombre)
-    jugador_objeto = Usuario.objects.filter(username=jugador_o.username)#select * from Reto where nombre = jugador_nombre
+    print(jugador_o[0].username)
+    #jugador_objeto = Usuario.objects.filter(username=jugador_o[0].username)#select * from Reto where nombre = jugador_nombre
     jugador_json = serializers.serialize('json',jugador_objeto )
 
     nombreBD = jugador_objeto[0].username
