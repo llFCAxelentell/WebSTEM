0834467157822479b7a5521f2a69b6e72b09c270
llFCAxelentell <72843024+llFCAxelentell@users.noreply.github.com>
2021-04-21 21:11:39 -0500

    Update views.py

--- WEB/views.py
+++ WEB/views.py
@@ -22,11 +22,11 @@ def formulario(request):
     nickname = request.POST['nickname']
     
     if (nombre == "" or apellido == "" or edad == "" or genero == "" or correo == "" or contrasena == "" or nickname == ""):
-        return HttpResponse("<p>Faltan uno o más datos</p>") 
+        return
     else:
         guardar = Usuario(names= nombre, last_names=apellido, created= dt.now(), email= correo, password=contrasena,username= nickname, gender= genero, birthdate= edad)
         guardar.save()
-        return HttpResponse("<p>Usuario registardo exitosamente</p>")
+        return 
 
 def index(request):
     return render(request, 'index.html')
