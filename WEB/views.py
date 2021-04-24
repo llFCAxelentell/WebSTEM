4dfbfebab05697bca598ca5d8cd37d96a00dbd20
Yusdivia Molina <yus.molina@gmail.com>
2021-04-24 17:28:35 -0500

    Update views.py

--- WEB/views.py
+++ WEB/views.py
@@ -100,7 +100,7 @@ def mi_estadistica(request):
             database = "medchembd"
         )
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM auth_user WHERE auth_user.username ='%s';", uu)
+        cursor.execute("SELECT * FROM auth_user WHERE auth_user.username =%s;", uu)
         rows = cursor.fetchall()
         print(rows)
 
