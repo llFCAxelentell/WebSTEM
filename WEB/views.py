3541d4556a52b8f822f23bb546400e0b8885cbe9
Yusdivia Molina <yus.molina@gmail.com>
2021-04-23 19:19:12 -0500

    Update views.py

--- WEB/views.py
+++ WEB/views.py
@@ -317,7 +317,7 @@ def estadistica(request):
         cursor.execute("SELECT extract (epoch from (ended::timestamp - started::timestamp))::integer/60 AS TiempoSesion FROM \"WEB_sesion\";")
 
         cursor2.execute("SELECT SUM(num_compounds_made) FROM \"WEB_day\" INNER JOIN \"WEB_try\" ON \"WEB_day\".try_id_id=\"WEB_try\".id INNER JOIN \"WEB_sesion\" ON \"WEB_try\".session_id_id= \"WEB_sesion\".id GROUP BY \"WEB_try\".session_id_id;")
-        cursor3.execute("SELECT extract (epoch from (ended::timestamp - started::timestamp))::integer/60 AS TiempoSesion FROM \"WEB_sesion\";")
+        cursor3.execute("SELECT extract (epoch from (ended::timestamp - started::timestamp))::integer/60 FROM \"WEB_sesion\";")
 
         #cursor3.execute("SELECT extract (epoch from (ended::timestamp - started::timestamp))::integer/60 AS Tiempo FROM \"WEB_sesion\" INNER JOIN \"WEB_usuario\" WHERE \"WEB_sesion\".user_id_id=\"WEB_usuario\".id;")
         #GROUP BY Edad
