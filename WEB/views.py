84580ef2a78be785d07b3b66968247233eba7eb8
Yusdivia Molina <yus.molina@gmail.com>
2021-04-24 17:16:57 -0500

    Update views.py

--- WEB/views.py
+++ WEB/views.py
@@ -52,7 +52,7 @@ def mi_estadistica(request):
 
     try:
         usuario = request.user
-        \set uu request.user
+        #\set uu request.user
         registros = User.objects.filter(username=usuario)
         print(registros)
         regist = Usuario.objects.filter(username=registros[0].id)
@@ -100,7 +100,7 @@ def mi_estadistica(request):
             database = "medchembd"
         )
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM auth_user WHERE user = :uu.auth_user;")
+        cursor.execute("SELECT * FROM auth_user WHERE user = \"hola\";")
         rows = cursor.fetchall()
         print(rows)
 
