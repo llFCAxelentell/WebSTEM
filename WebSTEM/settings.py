7e21a88d72ae3f4fe19b7b981da7572aea33bc87
Yusdivia Molina <yus.molina@gmail.com>
2021-04-25 11:02:37 -0500

    n

--- WebSTEM/settings.py
+++ WebSTEM/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.redirects',
      'WEB',
 ]
 
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.redirects.middleware.RedirectFallbackMiddleware',
 ]
 
 ROOT_URLCONF = 'WebSTEM.urls'
